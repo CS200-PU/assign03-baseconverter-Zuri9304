084e7ed96465b4b66e150f995cb01c141d5315e9
Zuri9304 <112516115+Zuri9304@users.noreply.github.com>
2024-09-21 22:59:56 +0000

    Working

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,89 @@
 //******************************************************************************
 // File name:   main.cpp
 // Author:      CS Faculty
-// Date:        xx/xx/xxxx (Replace xx/xx/xxxx with month/day/year)
-// Class:       CS150-xx (Replace xx with your section number)
-// Assignment:  Hello World
-// Purpose:     Display the message Hello World to the screen
-// Hours:       0.25
+// Date:        09/21/2024 
+// Class:       CS200
+// Assignment:  Base Converter
+// Purpose:     Conversion between Binary, Decimal, and Hexidecimal
+// Hours:       0.25 lol
 //******************************************************************************
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main () {
+// FUNCTION DECLARATIONS
+void printTitle(const string& myTitle);
+string getNumber(const string& prompt);
+char getBase (const string& strNumber);
+int hexCharToInt (char hexDigit);
 
-  cout << "Hello World" << endl;
-  
+
+int main () {
+  string const sTitle = "HEX-DECIMAL-BINARY CONVERTER";
+  string sPrompt = "\nEnter your string to convert (q to quit): ";
+ 
+  printTitle(sTitle);
+  getNumber(sPrompt);
+  while (getNumber(sPrompt) != "q"){
+
+
+
+    getNumber(sPrompt);
+  }
+
   return EXIT_SUCCESS;
+}
+/*
+
+FUNCTION printTitle
+
+*/
+void printTitle(const string& myTitle){
+  
+  cout << "**************************************\n";
+  cout << "*****" + myTitle + "*****\n";
+  cout << "**************************************\n";
+}
+/*
+
+FUNCTION getNumber
+
+*/
+string getNumber(const string& prompt){
+  string userInput;
+  cout << prompt;
+  cin >> userInput;
+
+  return userInput; 
+}
+/*
+
+getBase
+
+*/
+char getBase (const string& strNumber){ 
+  char letterVar;
+  
+  if (strNumber[1] == 'x'){
+    letterVar = 'H';
+  }
+  else if (strNumber[1] == 'b'){
+    letterVar = 'B';
+  }
+  else {
+    letterVar = 'D'; 
+  }
+
+  return letterVar;
+}
+/*
+
+FUNCTION hexChartoInt
+
+*/
+int hexCharToInt (char hexDigit){
+  
+
 }
